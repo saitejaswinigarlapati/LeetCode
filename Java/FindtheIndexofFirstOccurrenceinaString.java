17b89ea8b3de3e4b3e38da0f55dba0b8a8609743
Tejaswini <garlapatisaitejaswini@gmail.com>
2025-08-07 21:57:17 +0530

    Day 26

---
+++ Java/FindtheIndexofFirstOccurrenceinaString.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class FindtheIndexofFirstOccurrenceinaString {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0, j = needle.length(); j <= haystack.length(); i++, j++) {
+            if (haystack.substring(i, j).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the haystack string: ");
+        String haystack = sc.nextLine();
+
+        System.out.print("Enter the needle string: ");
+        String needle = sc.nextLine();
+
+        FindtheIndexofFirstOccurrenceinaString finder = new FindtheIndexofFirstOccurrenceinaString();
+        int index = finder.strStr(haystack, needle);
+
+        if (index != -1) {
+            System.out.println("The first occurrence is at index: " + index);
+        } else {
+            System.out.println("Needle not found in haystack.");
+        }
+
+    }
+}
---
+++ Python/FindtheIndexofFirstOccurrenceinaString.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self,haystack : str , needle: str) -> int:
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)]==needle:
+                return i
+        return -1
+s=Solution()
+
+haystack=input("Enter haystack : ")
+needle=input("Enter needle : ")
+print(s.strStr(haystack,needle))
+        
