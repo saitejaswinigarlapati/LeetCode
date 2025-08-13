db033189e63692caaee2a5e33eceade03e1bd483
Tejaswini <garlapatisaitejaswini@gmail.com>
2025-08-13 22:46:29 +0530

    Day 31

---
+++ Python/TreePostorder.py
@@ -0,0 +1,31 @@
+from typing import List,Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def postorder(node):
+            if not node:
+                return
+            postorder(node.left)
+            postorder(node.right)
+            res.append(node.val)
+        postorder(root)
+        return res
+    
+root = TreeNode(1)
+root.right = TreeNode(2)
+root.right.left = TreeNode(3)
+
+sol = Solution()
+print(sol.postorderTraversal(root))
+
+
+# Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+# Output: [4,6,7,5,2,9,8,3,1]
+
+# Input: root = [1,null,2,3]
+# Output: [3,2,1]
